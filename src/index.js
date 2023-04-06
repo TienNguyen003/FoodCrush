2e5dd83a35a4a998eac92875f6e95943986318e0
TienNguyen003 <dinhtien17082003@gmail.com>
2023-04-06 19:38:56 +0700

    Meals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Test from './App/test';
-import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
