b1490e4fc0b80e4ce6074b2ef22a37800601018e
TienNguyen003 <dinhtien17082003@gmail.com>
2023-04-06 18:58:01 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Test from './App/test';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Test></Test>
+);
