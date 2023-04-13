a0e1eaaad50e953205128fa762a8d8a60f856d86
TienNguyen003 <dinhtien17082003@gmail.com>
2023-04-13 23:26:27 +0700

    Meals

--- src/component/LayOuts/DefaultLayout/Header/header.js
+++ src/component/LayOuts/DefaultLayout/Header/header.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import { memo, useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Debounce from './debounce';
 import { SearchIcon } from '~/component/Icons/icon';
@@ -16,23 +17,21 @@ function Header({ parent }) {
 
     const sendData = () => {
         parent(debounce);
-    }
+    };
 
     useEffect(() => {
-            buttonRef.current.onclick = () => {
-                setInputValue(debounce);
-            };
+        buttonRef.current.onclick = () => {
+            setInputValue(debounce);
+        };
     }, [debounce]);
 
     return (
         <div className={cx('header')}>
             <div className={cx('logo')}>
-                <img
-                    className={cx('img')}
-                    src="https://i.imgur.com/aZxY6rG.png"
-                    alt="logo"
-                />
-                <h3 className={cx('name')}>FoodieCrush</h3>
+                <img className={cx('img')} src="https://i.imgur.com/aZxY6rG.png" alt="logo" />
+                <Link to={'/'} className={cx('name')}>
+                    FoodieCrush
+                </Link>
             </div>
             <div className={cx('search-meals')}>
                 <div className={cx('search')}>
@@ -42,11 +41,7 @@ function Header({ parent }) {
                         className={cx('search-input')}
                         placeholder="For example: American..."
                     />
-                    <div
-                        className={cx('icon')}
-                        ref={buttonRef}
-                        onClick={sendData}
-                    >
+                    <div className={cx('icon')} ref={buttonRef} onClick={sendData}>
                         <SearchIcon className={cx('icon-search')} />
                     </div>
                 </div>
