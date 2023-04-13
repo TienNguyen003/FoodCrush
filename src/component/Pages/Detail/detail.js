64023bc74819ced9e121eac76c9585523fd94f30
TienNguyen003 <dinhtien17082003@gmail.com>
2023-04-13 23:16:46 +0700

    Meals

--- src/component/Pages/Detail/detail.js
+++ src/component/Pages/Detail/detail.js
@@ -4,7 +4,6 @@ import styles from './detail.module.scss';
 const cx = classNames.bind(styles);
 
 function Detail() {
-    // "homepage": "https://tiennguyen003.github.io/WeatherMe/",
     return <h1 className={cx('test')}>sao lai deo duoc vay dit con me may</h1>;
 }
 
