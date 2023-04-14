63273e4925ffc8e05e0174df8db3e8a2735a5b22
TienNguyen003 <dinhtien17082003@gmail.com>
2023-04-14 22:27:41 +0700

    Meals

--- src/component/Pages/Detail/detail.js
+++ src/component/Pages/Detail/detail.js
@@ -6,6 +6,8 @@ const cx = classNames.bind(styles);
 
 function Detail() {
     const [dataAPI, setDataAPI] = useState([]);
+    const [ingredient, setIngredient] = useState([]);
+    const [spice, setSpice] = useState([]);
 
     let currentUrl = window.location.href;
     let newURL;
@@ -13,25 +15,24 @@ function Detail() {
         newURL = currentUrl.replace('http://localhost:3001/@', '');
     }
     useEffect(() => {
+        let arrIngredient = [];
+        let arrSpice = [];
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${newURL}`)
             .then((res) => res.json())
-            .then((data) => setDataAPI(data.meals[0]));
+            .then((data) => {
+                setDataAPI(data.meals[0]);
+                Object.keys(data.meals[0]).forEach((key) => {
+                    if (key.includes('strIngredient') && data.meals[0][key] !== '') {
+                        arrIngredient.push(data.meals[0][key]);
+                        setIngredient(arrIngredient);
+                    }
+                    if (key.includes('strMeasure') && data.meals[0][key] !== '') {
+                        arrSpice.push(data.meals[0][key]);
+                        setSpice(arrSpice);
+                    }
+                });
+            });
     }, [newURL]);
-    
-    console.log(dataAPI);
-
-    const arr = [
-        'chicken',
-        'chicken1',
-        'chicken2',
-        'chicken3',
-        'chicken4',
-        'chicken5',
-        'chicken6',
-        'chicken7',
-        'chicken8',
-        'chicken9',
-    ];
 
     return (
         <div className={cx('detail-meal')}>
@@ -42,12 +43,25 @@ function Detail() {
             <div className={cx('meal-recipe')}>
                 <h3>Công thức nấu ăn</h3>
                 <div className={cx('meal-desc')}>{dataAPI.strInstructions}</div>
+                <h3 className={cx('ingredient')}>Nguyên liệu cần chuẩn bị</h3>
                 <div className={cx('meal-ingredient')}>
-                    {arr.map((item, index) => {
+                    {ingredient.map((item, index) => {
                         return (
                             <div key={index}>
                                 <p>
                                     {index + 1}. {item}
+                                </p>
+                            </div>
+                        );
+                    })}
+                </div>
+                <h3 className={cx('ingredient')}>Gia vị cần chuẩn bị</h3>
+                <div className={cx('meal-ingredient')}>
+                    {spice.map((item, index) => {
+                        return (
+                            <div key={index}>
+                                <p>
+                                    {index + 1}: {item}
                                 </p>
                             </div>
                         );
