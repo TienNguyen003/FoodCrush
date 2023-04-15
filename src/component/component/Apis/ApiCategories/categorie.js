01bcbd9a8145b18055984002341c0e7a78870596
TienNguyen003 <dinhtien17082003@gmail.com>
2023-04-15 22:48:49 +0700

    Meals

--- src/component/component/Apis/ApiCategories/categorie.js
+++ src/component/component/Apis/ApiCategories/categorie.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import { useEffect, useState } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 
 import styles from './categorie.module.scss';
 import Button from '~/component/LayOuts/DefaultLayout/Button/button';
@@ -11,17 +11,26 @@ function Categories() {
     const [dataCategories, setDataCategories] = useState([]);
 
     useEffect(() => {
-        let arrBtn = [];
-        fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
-            .then((res) => res.json())
-            .then((data) => {
-                data.meals.map((item) => arrBtn.push(item.strCategory));
-                setBtnCategories(arrBtn);
-            });
-        fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-            .then((res) => res.json())
-            .then((data) => {
-                setDataCategories(data.categories.slice(0, 9));
+            fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+                .then((res) => res.json())
+                .then((data) => {
+                    setBtnCategories(data.meals);
+                });
+
+            const btnMeal = document.querySelectorAll('.categorie_btn-meal-item__NS6Im');
+            btnMeal.forEach((btn) => {
+                btn.onclick = () => {
+                    for (let i = 0; i < btnMeal.length; i++) {
+                        btnMeal[i].classList.remove('Button_primary__Wl-ml');
+                    }
+                    btn.classList.add('Button_primary__Wl-ml');
+
+                    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${btn.innerText}`)
+                        .then((res) => res.json())
+                        .then((data) => {
+                            setDataCategories(data.meals.slice(0, 9));
+                        });
+                };
             });
     }, []);
 
@@ -30,8 +39,12 @@ function Categories() {
             <div className={cx('bnt-meal')}>
                 {btnCategories.map((btn, index) => {
                     return (
-                        <Button className={cx('btn-meal-item')} outline rounded key={index}>
-                            {btn}
+                        <Button
+                            className={cx('btn-meal-item', 'animate__animated animate__rubberBand')}
+                            rounded
+                            key={index}
+                        >
+                            {btn.strCategory}
                         </Button>
                     );
                 })}
@@ -39,8 +52,15 @@ function Categories() {
             <div className={cx('categorie')}>
                 {dataCategories.map((item) => {
                     return (
-                        <div key={item.idCategory} className={cx('categories-item')}>
-                            <img className={cx('img')} src={item.strCategoryThumb} alt="" />
+                        <div
+                            key={item.idMeal}
+                            className={cx(
+                                'categories-item',
+                                'animate__animated animate__rubberBand animate__repeat-2',
+                            )}
+                        >
+                            <img className={cx('img')} src={item.strMealThumb} alt="" />
+                            <p className={cx('name-meal')}>{item.strMeal}</p>
                         </div>
                     );
                 })}
@@ -49,4 +69,4 @@ function Categories() {
     );
 }
 
-export default Categories;
+export default memo(Categories);
